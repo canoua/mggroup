4ae228b15e7fc3b630cf574b5892618d9cd36f35
Andrew-Karyaka <akaryaka1997@gmail.com>
2023-02-24 18:10:08 +0600

    refactoring

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,18 @@
-console.log('test');
+import Swiper, { Navigation, Pagination, Scrollbar  } from 'swiper';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/scrollbar';
+
+// Swiper.use([Navigation, Pagination, Scrollbar]);
+
+const swiper = new Swiper(".swiper", {
+  grabCursor: true,
+  loop: true,
+  modules: [Navigation, Pagination, Scrollbar],
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+    hide: true
+  },
+});
